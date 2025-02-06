024a0c8fee13c08b35866e4820cb5bad58658249
omgitsmoe <60219950+omgitsmoe@users.noreply.github.com>
2025-02-06 21:48:00 +0100

    Add: packages

---
+++ checksum_helper/src/gather.rs
@@ -0,0 +1,4 @@
+pub struct Gather {};
+
+impl Gather {
+};
---
+++ checksum_helper_cli/src/main.rs
@@ -0,0 +1,7 @@
+use checksum_helper;
+
+fn main() {
+    let n1 = 2;
+    let n2 = 6;
+    println!("Hello, world! {n1} + {n2} is {}!", checksum_helper::add(n1, n2));
+}
